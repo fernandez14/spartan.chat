3c2a895716e69f55c4d281c9d5e24268a99e930e
fernandez14 <fernandez14@outlook.com>
2017-06-06 11:21:26 -0500

    Chat message aggregation.

--- app/chat.js
+++ app/chat.js
@@ -1,4 +1,4 @@
-import {div, h1, input, ul, li} from '@cycle/dom';
+import {div, h1, input, ul, li, img, span, p} from '@cycle/dom';
 import xs from 'xstream';
 import debounce from 'xstream/extra/debounce';
 
@@ -22,7 +22,7 @@ function intent(dom, socket) {
 
         // Watch scroll on message list.
         dom.select('.list-container').events('scroll')
-            .compose(debounce(100))
+            .compose(debounce(50))
             .map(e => ({type: 'feed-scroll', top: e.target.scrollTop, height: e.target.scrollHeight - e.target.clientHeight})),
 
         // Simulate periodic messages.
@@ -37,7 +37,7 @@ function intent(dom, socket) {
  */
 function model(actions$) {
     const messages$ = actions$.filter(a => a.type == 'others-message' || (a.type == 'message' && a.sent))
-        .map(a => ({message: a.payload.trim()}))
+        .map(a => ({message: a.payload.trim(), author: {id: 'unknown', name: 'nobody', avatar: 'http://via.placeholder.com/40x40'}}))
         .fold((acc, c) => acc.concat(c), [])
         .map(list => list.slice(-50));
 
@@ -64,8 +64,10 @@ function view(state$) {
         return div('.mw7.center.sans-serif', [
             h1('.tc', 'SpartanGeek'),
             div('.ba.pa3.h6.b--silver.overflow-auto.list-container', {style: {maxHeight: '250px'}}, [
-                ul('.list.pa0.ma0', state.list.map(item => {
-                    return li('.pv2', {
+                ul('.list.pa0.ma0', state.list.map((item, index, list) => {
+                    const simple = index > 0 && list[index-1].author.id == item.author.id;
+
+                    return li('.dt' + (simple == false ? '.pv2' : '.pb2'), {
                         hook: {
                             insert: vnode => {
                                 if (state.lock) {
@@ -73,7 +75,13 @@ function view(state$) {
                                 }
                             }
                         }
-                    }, item.message);
+                    }, [
+                        div('.dtc.w2', simple == false ? img({attrs: {src: item.author.avatar}}) : ''),
+                        div('.dtc.v-top.pl3', [
+                            simple == false ? span('.f6.f5-ns.fw6.lh-title.black.db.mb1', item.author.name) : '',
+                            p('.f6.fw4.mt0.mb0.black-60', item.message)
+                        ])
+                    ]);
                 }))
             ]),
             div('.pv3', [

4307cd7a4f7b327051775732c706295d13db0b01
fernandez14 <fernandez14@outlook.com>
2017-07-08 18:25:40 -0500

    Slice messages.

--- app/chat/model.js
+++ app/chat/model.js
@@ -109,7 +109,7 @@ export function model(actions) {
         .map(packed => state => {
             const channel = packed.channel || false;
 
-            return {...state, list: channel == false || state.channel == channel || channel === 'log' ? state.list.concat(packed.list) : state.list, missing: state.lock === false ? state.missing + packed.list.length : state.missing};
+            return {...state, list: channel == false || state.channel == channel || channel === 'log' ? state.list.concat(packed.list).slice(-100) : state.list, missing: state.lock === false ? state.missing + packed.list.length : state.missing};
         });
 
     /**
